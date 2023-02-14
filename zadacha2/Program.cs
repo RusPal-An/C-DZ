e55014b78531e08214d2e6f842309e134d2ea35e
RusPal-An <paltsev_ruslan@Mail.ru>
2023-02-14 16:32:15 +0300

    add zadacha 2

---
+++ zadacha2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.Write("Введите  число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a < b)
+    Console.WriteLine("a min, b max ");
+else Console.WriteLine("a max, b min ");    
