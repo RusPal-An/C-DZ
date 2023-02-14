7f65cbf1dc96bedf1bde75c4b5ac6663cf1cfeb6
RusPal-An <paltsev_ruslan@Mail.ru>
2023-02-14 16:52:39 +0300

    add zadacha 4

---
+++ zadacha4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите 1-е число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if(a<b<c);
+    
