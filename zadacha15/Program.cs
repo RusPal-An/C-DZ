f5521abad35799cd9b4c0b111844536842ebcd2a
RusPal-An <paltsev_ruslan@Mail.ru>
2023-02-16 12:51:52 +0300

    add zadacha 15

---
+++ zadacha15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Введите день недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись!\nВведите день недели: ");
+    n = Convert.ToInt32(Console.ReadLine());
+
+}
+if ( n == 6 || n == 7 );
+    Console.WriteLine("no");
+else
+    Console.WriteLine("yes");
