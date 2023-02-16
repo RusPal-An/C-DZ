5ca4d6141363502efca0ffcb1ea767971277dc01
RusPal-An <paltsev_ruslan@Mail.ru>
2023-02-16 12:19:18 +0300

    add zadacha 10

---
+++ zadacha10/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+
+Console.Write("Введите трёхзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int n1 = (n / 10);
+Console.WriteLine(n1%10);
--- zadacha4/Program.cs
+++ zadacha4/Program.cs
@@ -4,3 +4,9 @@ Console.Write("Введите 2-е число: ");
 int b = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 3-е число: ");
 int c = Convert.ToInt32(Console.ReadLine());
+if(a>b || b>c)
+    Console.WriteLine($"{a} - максимальное число ");
+else 
+    Console.WriteLine($"{b} - максимальное число ");   
+else(c>a || c>b);
+    Console.WriteLine($"{c} - максимальное число ");  
